d26ec2ce971f4d45aedadf455f2c0f5c5cc2fec9
Daniel Valdivia <hola@danielvaldivia.com>
2019-09-20 17:34:42 -0700

    Add react app for UI

---
+++ webui/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>mcs</h1>
+                <p>
+                    Coming soon
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
