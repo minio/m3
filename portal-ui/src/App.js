208512370bd4d34f1007be85ae29ceb5e1689378
Lenin Alevski Huerta Arias <alevsk.8772@gmail.com>
2019-10-09 15:03:10 -0700

    Bootstrap portal-ui using latest version of create-react-app

---
+++ portal-ui/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Create React App - 3.2.0
+    </div>
+  );
+}
+
+export default App;
---
+++ portal-ui/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { BrowserRouter } from "react-router-dom";
+import configureStore from './store';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+ );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
