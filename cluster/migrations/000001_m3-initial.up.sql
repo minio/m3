0c78dda017c5d9081dc1ae04fb7c44b602e3e34c
Daniel Valdivia <hola@danielvaldivia.com>
2019-10-22 16:34:02 -0700

    Updates to the schema

--- cluster/migrations/000001_m3-initial.up.sql
+++ cluster/migrations/000001_m3-initial.up.sql
@@ -2,97 +2,89 @@ create schema provisioning;
 
 create table provisioning.tenants
 (
-    id uuid not null
+    id         uuid         not null
         constraint tenants_pk
             primary key,
-    name varchar(256) not null,
+    name       varchar(256) not null,
     short_name varchar(256) not null
 );
 
-alter table provisioning.tenants owner to postgres;
 
 create table provisioning.storage_groups
 (
-    id uuid not null
+    id   uuid   not null
         constraint storage_groups_pk
             primary key,
     name varchar(256),
-    num serial not null
+    num  serial not null
 );
 
-alter table provisioning.storage_groups owner to postgres;
 
 create table provisioning.tenants_storage_groups
 (
-    tenant_id uuid not null
+    tenant_id        uuid        not null
         constraint tenants_storage_groups_tenants_id_fk
             references provisioning.tenants,
-    storage_group_id uuid not null
+    storage_group_id uuid        not null
         constraint tenants_storage_groups_storage_groups_id_fk
             references provisioning.storage_groups,
-    port integer not null,
-    service_name varchar(64) not null
+    port             integer     not null,
+    service_name     varchar(64) not null
 );
 
-alter table provisioning.tenants_storage_groups owner to postgres;
 
 create table provisioning.nodes
 (
-    id uuid not null
+    id        uuid not null
         constraint nodes_pk
             primary key,
-    name varchar(256),
+    name      varchar(256),
     k8s_label varchar(256)
 );
 
-alter table provisioning.nodes owner to postgres;
 
 create table provisioning.storage_clusters
 (
-    id uuid not null
+    id   uuid not null
         constraint storage_clusters_pk
             primary key,
     name varchar(256)
 );
 
-alter table provisioning.storage_clusters owner to postgres;
 
 create table provisioning.storage_clusters_groups
 (
     storage_cluster_id uuid not null
         constraint storage_clusters_groups_storage_clusters_id_fk
             references provisioning.storage_clusters,
-    storage_group_id uuid not null
+    storage_group_id   uuid not null
         constraint storage_clusters_groups_storage_groups_id_fk
             references provisioning.storage_groups
 );
 
-alter table provisioning.storage_clusters_groups owner to postgres;
 
 create table provisioning.storage_cluster_nodes
 (
     storage_cluster_id uuid not null
         constraint storage_cluster_nodes_storage_clusters_id_fk
             references provisioning.storage_clusters,
-    node_id uuid not null
+    node_id            uuid not null
         constraint storage_cluster_nodes_nodes_id_fk
             references provisioning.nodes,
-    k8s_label varchar(256)
+    k8s_label          varchar(256)
 );
 
-alter table provisioning.storage_cluster_nodes owner to postgres;
 
 create table provisioning.node_volumes
 (
-    id uuid not null
+    id         uuid not null
         constraint node_volumes_pk
             primary key,
-    node_id uuid not null
+    node_id    uuid not null
         constraint node_volumes_nodes_id_fk
             references provisioning.nodes,
     mount_path varchar(256)
 );
 
-alter table provisioning.node_volumes owner to postgres;
 
 
--- cluster/tenant-migrations/000001_tenant-initial.up.sql
+++ cluster/tenant-migrations/000001_tenant-initial.up.sql
@@ -1,177 +1,114 @@
-create table users
+create table atenantschema.users
 (
-    id uuid not null
+    id           uuid                                   not null
         constraint users_pk
             primary key,
-    full_name varchar(256),
-    email varchar(256) not null,
-    password varchar(256),
-    is_admin boolean default false,
-    from_idp boolean
+    full_name    varchar(256),
+    email        varchar(256)                           not null,
+    password     varchar(256),
+    is_admin     boolean                  default false,
+    from_idp     boolean,
+    created_date timestamp with time zone default now() not null
 );
 
-alter table users owner to postgres;
 
 create unique index users_email_uindex
-    on users (email);
+    on atenantschema.users (email);
 
-create index users_hashed_password_index
-    on users (password);
+create index users_password_index
+    on atenantschema.users (password);
 
-create table service_accounts
+create table atenantschema.service_accounts
 (
-    id uuid not null
+    id           uuid                                   not null
         constraint service_accounts_pk
             primary key,
-    name varchar(256) not null,
-    created_by varchar(256) not null,
-    created_date timestamp with time zone default now() not null,
-    description text
-);
-
-alter table service_accounts owner to postgres;
-
-create table token_groups
-(
-    id uuid not null
-        constraint token_groups_pk
-            primary key,
-    name varchar(256) not null,
-    description text,
-    created_by varchar(256) not null,
+    name         varchar(256)                           not null,
+    description  text,
+    created_by   varchar(256)                           not null,
     created_date timestamp with time zone default now() not null
 );
 
-alter table token_groups owner to postgres;
 
-create table tokens
+create table atenantschema.credentials
 (
-    access_key varchar(256) not null
-        constraint tokens_pk
+    access_key         varchar(256)                           not null
+        constraint credentials_pk
             primary key,
-    user_id uuid
-        constraint tokens_users_id_fk
-            references users
+    user_id            uuid
+        constraint credentials_users_id_fk
+            references atenantschema.users
             on delete cascade,
     service_account_id uuid
-        constraint tokens_service_accounts_id_fk
-            references service_accounts
+        constraint credentials_service_accounts_id_fk
+            references atenantschema.service_accounts
             on delete cascade,
-    created_by varchar(256) not null,
-    created_date timestamp with time zone default now() not null,
-    ui_token boolean default false,
-    token_group_id uuid not null
-        constraint tokens_token_groups_id_fk
-            references token_groups
-            on delete cascade
+    ui_credential      boolean                  default false,
+    created_by         varchar(256)                           not null,
+    created_date       timestamp with time zone default now() not null
 );
 
-alter table tokens owner to postgres;
 
-create table policies
+create table atenantschema.permissions
 (
-    name varchar(256) not null,
-    created_by varchar(256) not null,
-    created_date timestamp with time zone default now() not null,
-    id uuid not null
-        constraint policies_pk
-            primary key
-);
-
-alter table policies owner to postgres;
-
-create table groups
-(
-    id uuid not null
-        constraint groups_pk
-            primary key,
-    name varchar(256) not null,
-    policy_id uuid not null
-        constraint groups_policies_id_fk
-            references policies
-            on delete cascade,
-    created_by varchar(256) not null,
-    created_date timestamp with time zone default now() not null
-);
-
-alter table groups owner to postgres;
-
-create table users_groups
-(
-    user_id uuid not null
-        constraint users_groups_users_id_fk
-            references users
-            on delete cascade,
-    group_id uuid not null
-        constraint users_groups_groups_id_fk
-            references groups
-            on delete cascade,
-    created_by varchar(256) not null,
-    created_date timestamp with time zone default now() not null
-);
-
-alter table users_groups owner to postgres;
-
-create table policy_statements
-(
-    id uuid not null
+    id           uuid                                   not null
         constraint policy_statements_pk
             primary key,
-    policy_id uuid
-        constraint policy_statements_policies_id_fk
-            references policies
-            on delete cascade,
-    created_by varchar(256) not null,
-    created_date timestamp with time zone default now() not null,
-    effect varchar(64) not null
-);
-
-alter table policy_statements owner to postgres;
-
-create table policy_statement_actions
-(
-    id uuid not null
-        constraint policy_statement_actions_pk
-            primary key,
-    statement_id uuid
-        constraint policy_statement_actions_policy_statements_id_fk
-            references policy_statements
-            on delete cascade,
-    action varchar(256) not null,
-    created_by varchar(256) not null,
+    effect       varchar(64)                            not null,
+    created_by   varchar(256)                           not null,
     created_date timestamp with time zone default now() not null
 );
 
-alter table policy_statement_actions owner to postgres;
 
-create table policy_statement_resources
+create table atenantschema.permissions_resources
 (
-    id uuid not null
+    id           uuid                                   not null
         constraint policy_statement_resources_pk
             primary key,
     statement_id uuid
         constraint policy_statement_resources_policy_statements_id_fk
-            references policy_statements,
-    resource varchar(512) not null,
-    created_by varchar(256) not null,
+            references atenantschema.permissions,
+    resource     varchar(512)                           not null,
+    created_by   varchar(256)                           not null,
     created_date timestamp with time zone default now() not null
 );
 
-alter table policy_statement_resources owner to postgres;
 
-create table service_account_groups
+create table atenantschema.service_accounts_permissions
 (
-    service_account_id uuid not null
-        constraint service_account_groups_service_accounts_id_fk
-            references service_accounts
+    service_account_id uuid                                   not null
+        constraint service_accounts_permissions_service_accounts_id_fk
+            references atenantschema.service_accounts
             on delete cascade,
-    group_id uuid not null
-        constraint service_account_groups_groups_id_fk
-            references groups
+    permission_id      uuid                                   not null
+        constraint service_accounts_permissions_permissions_id_fk
+            references atenantschema.permissions
             on delete cascade,
-    created_by varchar(256) not null,
-    created_date timestamp with time zone default now() not null
+    created_by         varchar(256)                           not null,
+    created_date       timestamp with time zone default now() not null
 );
 
-alter table service_account_groups owner to postgres;
+
+create table atenantschema.actions
+(
+    id          uuid not null
+        constraint actions_pk
+            primary key,
+    name        varchar(256),
+    description text
+);
+
+
+create table atenantschema.permissions_actions
+(
+    permission_id uuid
+        constraint permissions_actions_permissions_id_fk
+            references atenantschema.permissions
+            on delete cascade,
+    action_id     uuid                                   not null
+        constraint permissions_actions_actions_id_fk
+            references atenantschema.actions,
+    created_by    varchar(256)                           not null,
+    created_date  timestamp with time zone default now() not null
+);
 
