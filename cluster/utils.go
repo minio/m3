a0515d344db1fa0f1c668562c8b4f995d8efa16a
Daniel Valdivia <hola@danielvaldivia.com>
2019-10-25 20:42:38 -0700

    Switch to recommended algorithm

--- cluster/utils.go
+++ cluster/utils.go
@@ -18,21 +18,35 @@ package cluster
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
 
-const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const letterBytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+var src = rand.NewSource(time.Now().UnixNano())
 
-func StringWithCharset(length int, charset string) string {
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandomKeyWithLength(n int) string {
+	sb := strings.Builder{}
+	sb.Grow(n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			sb.WriteByte(letterBytes[idx])
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
 	}
-	return string(b)
-}
 
-func RandomKeyWithLength(length int) string {
-	return StringWithCharset(length, charset)
+	return sb.String()
 }
