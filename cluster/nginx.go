8a6e7e556156578b0bac4aed374ed308e0670848
Lenin Alevski <alevsk.8772@gmail.com>
2019-11-11 21:16:03 -0800

    nginx-resolver-no-drop-connections - spawning new nginx and deleting previous deployment versions

--- cluster/nginx.go
+++ cluster/nginx.go
@@ -19,33 +19,35 @@ package cluster
 import (
 	"bytes"
 	"fmt"
+	"strings"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/api/extensions/v1beta1"
-	k8errors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 )
 
 const (
-	nginxLBName     = "nginx-resolver"
-	nginxLBAppLabel = "nginx-resolver"
+	nginxLBName = "nginx-resolver"
 )
 
 var (
-	nginxLBReplicas   = int32(1)
-	nginxLBDeployment = v1beta1.Deployment{
+	nginxResolverVersion = "m1337"
+	nginxLBReplicas      = int32(1)
+	nginxLBDeployment    = v1beta1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: nginxLBName,
+			Name: fmt.Sprintf(`%s-%s`, nginxLBName, nginxResolverVersion),
 		},
 		Spec: v1beta1.DeploymentSpec{
 			Replicas: &nginxLBReplicas,
 			Template: v1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
-						"app": nginxLBAppLabel,
+						"app":  fmt.Sprintf(`%s-%s`, nginxLBName, nginxResolverVersion),
+						"type": "nginx-resolver",
 					},
 				},
 				Spec: nginxLBPodSpec,
@@ -91,33 +93,19 @@ var (
 	}
 )
 
-// ReDeployNginxResolver destroy current nginx deployment and replace it with a new want that will take latest configMap configuration
+// ReDeployNginxResolver destroy current nginx deployment and replace it with a new one that will take latest configMap configuration
 func ReDeployNginxResolver(ctx *Context) chan error {
 	ch := make(chan error)
 	go func() {
 		defer close(ch)
-		// creates the clientset
-		clientset, _ := k8sClient()
-		// does the deployment exist?
-		var shouldUpdate bool
-		_, err := extV1beta1API(clientset).Deployments("default").Get("nginx-resolver", metav1.GetOptions{})
-		switch {
-		case k8errors.IsNotFound(err):
-			shouldUpdate = false
-		case err != nil:
-			ch <- err
-			return
-		default:
-			shouldUpdate = true
-		}
-		DeployNginxResolver(shouldUpdate)
+		DeployNginxResolver(true)
 	}()
 	return ch
 }
 
-// DeleteNginxLBDeployment deletes the nginx-resolver deployment and indicates
+// DeleteNginxLBDeployments deletes the nginx-resolver old deployments and indicates
 // the completion of the deletion via the returned receiver channel
-func DeleteNginxLBDeployment(clientset *kubernetes.Clientset, deploymentName, appLabel string) <-chan struct{} {
+func DeleteNginxLBDeployments(clientset *kubernetes.Clientset, deploymentName string) <-chan struct{} {
 	doneCh := make(chan struct{})
 	go func() {
 		// Setup shared deployments informer on the default namespace to detect the
@@ -132,7 +120,7 @@ func DeleteNginxLBDeployment(clientset *kubernetes.Clientset, deploymentName, ap
 				// wait until the status of the deployment is known
 				case *v1beta1.Deployment:
 					fmt.Println(deployment.GetName(), deployment.GetLabels()["app"])
-					if v, ok := deployment.GetLabels()["app"]; ok && v == appLabel {
+					if v, ok := deployment.GetLabels()["app"]; ok && v != deploymentName {
 						fmt.Println("nginx deployment deleted")
 					}
 					// Signal the completion of deployment deletion
@@ -143,15 +131,26 @@ func DeleteNginxLBDeployment(clientset *kubernetes.Clientset, deploymentName, ap
 
 		go deploymentInformer.Run(doneCh)
 
-		// This delete option is to make sure that the pods belonging to
-		// the nginx-resolver deployment are 'collected' immediately.
-		fgPropagation := metav1.DeletePropagationForeground
-		fgDeleteOption := metav1.DeleteOptions{
-			PropagationPolicy: &fgPropagation,
-		}
-		err := extV1beta1API(clientset).Deployments("default").Delete(deploymentName, &fgDeleteOption)
+		labelSelector := metav1.LabelSelector{MatchLabels: map[string]string{"type": "nginx-resolver"}}
+		deployments, err := extV1beta1API(clientset).Deployments("default").List(metav1.ListOptions{
+			LabelSelector: labels.Set(labelSelector.MatchLabels).String(),
+		})
 		if err != nil {
-			close(doneCh) // the informer listening for delete deployment event needs to be stopped
+			close(doneCh)
+		}
+		for _, deployment := range deployments.Items {
+			if deployment.Name != deploymentName {
+				// This delete option is to make sure that the pods belonging to
+				// the nginx-resolver deployment are 'collected' immediately.
+				fgPropagation := metav1.DeletePropagationForeground
+				fgDeleteOption := metav1.DeleteOptions{
+					PropagationPolicy: &fgPropagation,
+				}
+				err := extV1beta1API(clientset).Deployments("default").Delete(deployment.Name, &fgDeleteOption)
+				if err != nil {
+					close(doneCh) // the informer listening for delete deployment event needs to be stopped
+				}
+			}
 		}
 	}()
 	return doneCh
@@ -169,24 +168,35 @@ func DeployNginxResolver(shouldUpdate bool) error {
 		return err
 	}
 	if shouldUpdate {
+		//Updating the version of the nginx-resolver
+		fmt.Println("creating nginx-resolver deployment with updated rules")
+		// nginxResolverOldVersion := nginxResolverVersion
+		nginxResolverVersion = fmt.Sprintf(`%s-%s`, nginxLBName, strings.ToLower(RandomCharString(6)))
+		nginxLBDeployment.Spec.Template.ObjectMeta.Labels["app"] = nginxResolverVersion
+		nginxLBDeployment.ObjectMeta.Name = nginxResolverVersion
+		if _, err = extV1beta1API(clientset).Deployments("default").Create(&nginxLBDeployment); err != nil {
+			return err
+		}
+		//Update nginx-resolver service to route traffic to the new nginx pods
+		nginxService, err := clientset.CoreV1().Services("default").Get("nginx-resolver", metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		nginxService.Spec.Selector["app"] = nginxResolverVersion
+		clientset.CoreV1().Services("default").Update(nginxService)
 		// Delete nginx-resolver deployment and wait until all its pods
 		// are deleted too. This is to ensure that the creation of the
 		// deployment results in new set of pods that have read the
 		// updated rules
-		waitCh := DeleteNginxLBDeployment(clientset, nginxLBName, nginxLBAppLabel)
+		waitCh := DeleteNginxLBDeployments(clientset, nginxResolverVersion)
 		// waiting for the delete of the nginx-resolver deployment to complete
 		<-waitCh
-
-		fmt.Println("creating nginx-resolver deployment with updated rules")
-		if _, err = extV1beta1API(clientset).Deployments("default").Create(&nginxLBDeployment); err != nil {
-			return err
-		}
 	} else {
 		if _, err = extV1beta1API(clientset).Deployments("default").Create(&nginxLBDeployment); err != nil {
 			return err
 		}
 	}
-	fmt.Println("done creating nginx-resolver deployment ")
+	fmt.Println("done creating nginx-resolver deployment")
 	return nil
 }
 
@@ -204,41 +214,48 @@ func UpdateNginxConfiguration(ctx *Context) chan error {
 		}
 		var nginxConfiguration bytes.Buffer
 		nginxConfiguration.WriteString(`
-user nginx;
-worker_processes auto;
-error_log /dev/stdout debug;
-pid /var/run/nginx.pid;
+			user nginx;
+			worker_processes auto;
+			error_log /dev/stdout debug;
+			pid /var/run/nginx.pid;
 
-events {
-	worker_connections  1024;
-}
+			events {
+				worker_connections  1024;
+			}
 
-http {
-log_format  main  '$http_host - $remote_addr - $remote_user [$time_local] "$request" '
-                               '$status $body_bytes_sent "$http_referer" '
-                               '"$http_user_agent" "$http_x_forwarded_for"';
-	server {
-		#listen 80 default_server;
-		#listen 443 ssl default_server;
-		server_name _ ;
-		return 404;
-	}
+			http {
+			log_format  main  '$http_host - $remote_addr - $remote_user [$time_local] "$request" '
+																		'$status $body_bytes_sent "$http_referer" '
+																		'"$http_user_agent" "$http_x_forwarded_for"';
+				server {
+					#listen 80 default_server;
+					#listen 443 ssl default_server;
+					server_name _ ;
+					return 404;
+				}
 		`)
 		for index := 0; index < len(tenantRoutes); index++ {
 			tenantRoute := tenantRoutes[index]
 			serverBlock := fmt.Sprintf(`
-	server {
-		server_name %s.s3.localhost;
-		location / {
-			proxy_pass http://%s:%d;
-		}
-	}
+				server {
+					server_name %s.s3.localhost;
+					ignore_invalid_headers off;
+					client_max_body_size 0;
+					proxy_buffering off;
+					location / {
+						proxy_http_version 1.1;
+						proxy_set_header Host $http_host;
+						proxy_read_timeout 15m;
+						proxy_request_buffering off;
+						proxy_pass http://%s:%d;
+					}
+				}
 
 			`, tenantRoute.ShortName, tenantRoute.ServiceName, tenantRoute.Port)
 			nginxConfiguration.WriteString(serverBlock)
 		}
 		nginxConfiguration.WriteString(`
-}
+			}
 		`)
 
 		configMap := v1.ConfigMap{
