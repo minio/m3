193506a23ae3d35ec975c1cf20525767c8a99608
Daniel Valdivia <hola@danielvaldivia.com>
2019-11-18 22:40:46 -0800

    Dashboard 2.0

--- k8s/create-kind.sh
+++ k8s/create-kind.sh
@@ -5,7 +5,7 @@ kind create cluster --name m3cluster --config kind-cluster.yaml
 echo "exporting KUBECONFIG"
 export KUBECONFIG="$(kind get kubeconfig-path --name="m3cluster")"
 echo "installing dashboard"
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta6/aio/deploy/recommended.yaml
 echo "creating service account"
 kubectl create serviceaccount dashboard -n default
 kubectl create clusterrolebinding dashboard-admin -n default --clusterrole=cluster-admin --serviceaccount=default:dashboard
