6e7b75cdc5492145ba8a5302dc57a1e846a98947
Lenin Alevski <alevsk.8772@gmail.com>
2019-12-06 16:31:18 -0800

    nginx-configmap - kubernetes configuration files to handle portal frontend/backend, kubernetes portal-proxy and more deployment files

---
+++ portal-backend/portal.go
@@ -0,0 +1,78 @@
+// This file is part of MinIO Kubernetes Cloud
+// Copyright (c) 2019 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package portal_backend
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	gw "github.com/minio/m3/api/stubs"
+	"github.com/rs/cors"
+	"google.golang.org/grpc"
+)
+
+func StartPortal() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Initialize Mux and transform request headers to grpc metadata
+	mux := runtime.NewServeMux(runtime.WithIncomingHeaderMatcher(func(h string) (string, bool) {
+		if strings.EqualFold(h, "sessionId") {
+			return h, true
+		}
+		return "", false
+	}))
+
+	m3Hostname := "localhost"
+	if os.Getenv("M3_HOSTNAME") != "" {
+		m3Hostname = os.Getenv("M3_HOSTNAME")
+	}
+	m3PublicPort := "50051"
+	if os.Getenv("M3_PUBLIC_PORT") != "" {
+		m3PublicPort = os.Getenv("M3_PUBLIC_PORT")
+	}
+	m3Address := fmt.Sprintf("%s:%s", m3Hostname, m3PublicPort)
+
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterPublicAPIHandlerFromEndpoint(ctx, mux, m3Address, opts)
+	if err != nil {
+		return err
+	}
+
+	//Set CORS allowed origins
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:5050"},
+		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodPatch},
+		// AllowCredentials indicates whether the request can include user credentials like
+		// cookies, HTTP authentication or client side SSL certificates.
+		AllowCredentials: true,
+		// Enable Debugging for testing, consider disabling in production
+		Debug:          true,
+		AllowedHeaders: []string{"Content-Type", "Sessionid"},
+	})
+
+	// Insert the middleware
+	handler := c.Handler(mux)
+	log.Println("Starting Portal server...")
+	return http.ListenAndServe(":5050", handler)
+}
