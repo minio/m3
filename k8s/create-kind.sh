e6bc13194e4fe794c46f759b1a8347d744804d19
Daniel Valdivia <hola@danielvaldivia.com>
2019-12-07 16:56:37 -0800

    Setup etcd cluster using etcd-operator
    Fix typo
    
    Speedup Setup
    
    Upgrade etcd to latest
    
    Fix comment

--- k8s/create-kind.sh
+++ k8s/create-kind.sh
@@ -7,6 +7,15 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-b
 echo "creating service account"
 kubectl create serviceaccount dashboard -n default
 kubectl create clusterrolebinding dashboard-admin -n default --clusterrole=cluster-admin --serviceaccount=default:dashboard
+# pre-load MinIO, postgres, etcd, etcd-operator to speed up setup
+docker pull minio/minio:RELEASE.2019-10-12T01-39-57Z
+docker pull postgres:12
+docker pull quay.io/coreos/etcd-operator:v0.9.4
+docker pull quay.io/coreos/etcd:v3.4.0
+kind load docker-image minio/minio:RELEASE.2019-10-12T01-39-57Z --name m3cluster
+kind load docker-image postgres:12 --name m3cluster
+kind load docker-image quay.io/coreos/etcd-operator:v0.9.4 --name m3cluster
+kind load docker-image quay.io/coreos/etcd:v3.2.13 --name m3cluster
 
 echo "done"
 
