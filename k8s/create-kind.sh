9575ee7e30c3f494cf5c4eeec656f37468283b01
Daniel Valdivia <hola@danielvaldivia.com>
2019-12-27 13:59:13 -0600

    kind updated minio release

--- k8s/create-kind.sh
+++ k8s/create-kind.sh
@@ -8,11 +8,11 @@ echo "creating service account"
 kubectl create serviceaccount dashboard -n default
 kubectl create clusterrolebinding dashboard-admin -n default --clusterrole=cluster-admin --serviceaccount=default:dashboard
 # pre-load MinIO, postgres, etcd, etcd-operator to speed up setup
-docker pull minio/minio:RELEASE.2019-12-19T22-52-26Z
+docker pull minio/minio:RELEASE.2019-12-24T23-04-45Z
 docker pull postgres:12
 docker pull quay.io/coreos/etcd-operator:v0.9.4
 docker pull quay.io/coreos/etcd:v3.4.0
-kind load docker-image minio/minio:RELEASE.2019-12-19T22-52-26Z --name m3cluster
+kind load docker-image minio/minio:RELEASE.2019-12-24T23-04-45Z --name m3cluster
 kind load docker-image postgres:12 --name m3cluster
 kind load docker-image quay.io/coreos/etcd-operator:v0.9.4 --name m3cluster
 kind load docker-image quay.io/coreos/etcd:v3.4.0 --name m3cluster
