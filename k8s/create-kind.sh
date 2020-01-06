ffffedc1ecdadda408bf384575d241747cff2400
Nitish Tiwari <nitish@minio.io>
2020-01-06 23:56:35 +0530

    Add Prometheus support for monitoring MinIO containers (#211)
    Co-authored-by: César Nieto <ces.nietor@gmail.com>
    Co-authored-by: Harshavardhana <harsha@minio.io>
    Co-authored-by: Daniel Valdivia <hola@danielvaldivia.com>

--- k8s/create-kind.sh
+++ k8s/create-kind.sh
@@ -12,17 +12,18 @@ docker pull minio/minio:RELEASE.2019-12-24T23-04-45Z
 docker pull postgres:12
 docker pull quay.io/coreos/etcd-operator:v0.9.4
 docker pull quay.io/coreos/etcd:v3.4.0
+docker pull quay.io/prometheus/prometheus:v2.14.0
 kind load docker-image minio/minio:RELEASE.2019-12-24T23-04-45Z --name m3cluster
 kind load docker-image postgres:12 --name m3cluster
 kind load docker-image quay.io/coreos/etcd-operator:v0.9.4 --name m3cluster
 kind load docker-image quay.io/coreos/etcd:v3.4.0 --name m3cluster
+kind load docker-image quay.io/prometheus/prometheus:v2.14.0 --name m3cluster
 
 make --directory="../portal-ui" k8sdev
 make --directory="../nginx" k8sdev
 kubectl apply -f deployments/m3-portal-backend-deployment.yaml
 kubectl apply -f deployments/m3-portal-frontend-deployment.yaml
 kubectl apply -f deployments/portal-proxy-deployment.yaml
-
 echo "done"
 
 # uncomment this if you want to use the static files
